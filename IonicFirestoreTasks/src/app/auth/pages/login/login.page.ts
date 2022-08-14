bac080feefcc01d2fef950e073908f137809b7b0
andregspaschoa <paschoa.andre@outlook.com>
2022-08-14 18:37:31 -0300

    ionic task

--- IonicFirestoreTasks/src/app/auth/pages/login/login.page.ts
+++ IonicFirestoreTasks/src/app/auth/pages/login/login.page.ts
@@ -1,4 +1,12 @@
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
+/* eslint-disable @typescript-eslint/member-ordering */
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +14,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  authForm: FormGroup;
+  configs = {
+    isSignIn: true,
+    action: 'Login',
+    actionChange: 'Create account'
+  };
+  private nameControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+  get name(): FormControl {
+    return <FormControl>this.authForm.get('email');
+  }
+  get email(): FormControl {
+    return <FormControl>this.authForm.get('email');
+  }
+
+  get password(): FormControl {
+    return <FormControl>this.authForm.get('password');
+  }
+  changeAuthAction(): void {
+    this.configs.isSignIn = !this.configs.isSignIn;
+    const { isSignIn } = this.configs;
+    this.configs.action = isSignIn ? 'Login' : 'Sign Up';
+    this.configs.actionChange = isSignIn ? 'Create account' : 'Already have an account';
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    !isSignIn
+      ? this.authForm.addControl('name', this.nameControl)
+      : this.authForm.removeControl('name');
+  }
+  onSubmit(): void {
+    console.log('AuthForm: ', this.authForm.value);
+  }
 }
