632280ba59b09b3ed766a36c7f3b8ab9a23e31d9
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2022-08-15 17:24:55 -0300

    Update environment.ts

--- IonicFirestoreTasks/src/environments/environment.ts
+++ IonicFirestoreTasks/src/environments/environment.ts
@@ -1,16 +1,17 @@
+/* eslint-disable @typescript-eslint/quotes */
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
   production: false, firebase : {
-    apiKey: 'AIzaSyCjTBQtxJoimpXkpaPFLxmsQnWQaC1KCDI',
-    authDomain: 'ionic-firestore-tasks-f226e.firebaseapp.com',
-    projectId: 'ionic-firestore-tasks-f226e',
-    storageBucket: 'ionic-firestore-tasks-f226e.appspot.com',
-    messagingSenderId: '385154921803',
-    appId: '1:385154921803:web:adc79de8864d6fd77564ce',
-    measurementId: 'G-WPC8L00BWM',
+    apiKey: "AIzaSyDc_G-Ju_I72T7STBfBdDCssdUIGi0Agxk",
+    authDomain: "gestor-de-tarefas-3b84a.firebaseapp.com",
+    projectId: "gestor-de-tarefas-3b84a",
+    storageBucket: "gestor-de-tarefas-3b84a.appspot.com",
+    messagingSenderId: "883386585054",
+    appId: "1:883386585054:web:527c245b5f5ec11c3f574f",
+    measurementId: "G-0WCSLTNRQ8"
   }
 };
 /*
